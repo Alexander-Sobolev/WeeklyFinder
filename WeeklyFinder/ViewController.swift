387c087bfe0c625475e4f549fe7cb9d2dfeb18c7
Alexander Sobolev <sobolev.ios.developer@gmail.com>
2020-12-17 15:45:39 +0300

    Added new Label and Button

--- WeeklyFinder/ViewController.swift
+++ WeeklyFinder/ViewController.swift
@@ -9,11 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var dateTF: UITextField!
+    @IBOutlet weak var monthTF: UITextField!
+    @IBOutlet weak var yearTF: UITextField!
+    @IBOutlet weak var resultLable: UILabel!
+    
+    
+    @IBAction func findDay(_ sender: UIButton) {
     }
-
-
+    
 }
 
